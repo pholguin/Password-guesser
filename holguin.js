7ecfce9dbcf209b69f5c89a48767696dd81e1d6b
Pamela Holguin <pholguin.student@hsfi.us>
2017-02-02 22:08:30 +0000

    i created a password

---
+++ holguin.js
@@ -0,0 +1,14 @@
+$('button').click(function(){
+  var name =$("input").val();
+  var lol = "linda"; 
+   if(name===lol){ 
+       $("div").html
+       ('CONGRATULATIONS!!!!!')
+       
+      
+   }else {
+        $("div").html
+       ('You are not smart.')
+       
+   }
+});
